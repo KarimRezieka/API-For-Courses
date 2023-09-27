f82588edb03e9561f06ad193cb43ba7387c59633
KarimRezieka <karimsaadrezieka@outlook.com>
2023-09-27 22:11:35 +0300

    post commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express =require('express')
+const {body,validationResult}= require('express-validator')
 const app = express()
 app.use(express.json())
 const courses = [
@@ -28,10 +29,11 @@ const courses = [
         price:3000
     },
 ]
-
+//get all courses
 app.get('/api/courses',(req,res)=>{
     res.json(courses);
 })
+//get a single course
 app.get('/api/courses/:courseId',(req,res)=>{
    const course = courses.find((course)=>course.id == req.params.courseId)
     if (!course) { 
@@ -40,9 +42,35 @@ app.get('/api/courses/:courseId',(req,res)=>{
    res.json(course)
 
 })
+// Create a new course 
+
+//old way to make validate
+/*
 app.post('/api/courses',(req,res)=>{
+    if(!req.body.title){
+        return res.status(400).json({error:'title not provided'})
+    }
+    if(!req.body.price){
+        return res.status(400).json({error:'price not provided'})
+    }
+    res.status(201).json(courses.push({...req.body}))
+ })*/
+
+// Create a new course  with using express validator
+app.post('/api/courses',[
+    body('title').notEmpty().withMessage("title is requires").isLength({min:2}).withMessage("title at least is 2 digits"),
+    body('price').notEmpty().withMessage("title at least is 2 digits")
+
+],(req,res)=>{
+    const errors = validationResult(req);
+    console.log("errors",errors);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array())
+    }
     res.status(201).json(courses.push({...req.body}))
 })
+
+
 
 app.listen(443,()=>{
     console.log('listening on port : 443')
